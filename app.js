5cabf4e34b2d4f45ea3dda99aa51d3e91e235414
Prash <gsa115376@gmail.com>
2024-08-26 00:57:41 +0530

    new file added

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const qes = require("./question.js");
+const path = require("path")
 
 const Mongo = "mongodb://127.0.0.1:27017/TestApp";
+
 
 main().then(() => {
     console.log("connected");
@@ -16,8 +18,16 @@ async function main() {
     await mongoose.connect(Mongo);
 }
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 app.get("/", (req,res) => {
     res.send("hi");
+});
+
+app.get('/your-route', (req, res) => {
+    res.render('new'); // This will look for 'new.ejs' in the views directory
 });
 
 // app.get("/testqes", async (req, res) => {
@@ -40,3 +50,25 @@ app.get("/", (req,res) => {
 app.listen(8080, () => {
     console.log("sucess");
 });
+
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// // Set the views directory
+// app.set('views', path.join(__dirname, 'views'));
+
+// // Set the view engine to EJS
+// app.set('view engine', 'ejs');
+
+// // Define a route
+// app.get('/your-route', (req, res) => {
+//     res.render('new'); // This will look for 'new.ejs' in the views directory
+// });
+
+// // Start the server
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
