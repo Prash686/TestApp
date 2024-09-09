f7d1aa00c569290cb9033994dacd2866d154bd21
Prash <gsa115376@gmail.com>
2024-09-09 18:59:27 +0530

    adding some new roots and making perfect features

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-const questions = require("./question.js");
-const subjects = require("./subjects.js");
-const courses = require("./course.js");
+const questions = require("./init/question.js");
+const subjects = require("./init/subjects.js");
+const courses = require("./init/course.js");
 const ejs = require("ejs");
 const ejsMate = require("ejs-mate");
 const path = require("path");
@@ -33,14 +33,15 @@ app.get("/", async (req, res) => {
     
 });
 
-app.get("/practice", async (req, res) => {
-    const allquestions = await questions.find({});
+app.get("/practice/:id", async (req, res) => {
+    let { id } = req.params;
+    const allquestions = await questions.find({subject: id});
     res.render("./practice.ejs", { allquestions });
 });
 
-app.get("/test", async (req, res) => {
-    const examquestions = await questions.find({});
-    const allSubjects = await questions.distinct('subject');
+app.get("/test/:id", async (req, res) => {
+    let { id } = req.params;
+    const examquestions = await questions.find({subject: id});
     let max = 296;
     const allquestions = [];
     for(i = 0 ; i < 20 ; i++){
@@ -56,19 +57,44 @@ app.get("/subjects", async (req, res) => {
     res.render("./subjects.ejs", { allSubjects });
 });
 
+
 app.get("/courses", async (req, res) => {
-    const allSubjects = await courses.find({});
-    res.render("./subjects.ejs", { allSubjects });
+    const allCourses = await courses.find({});
+    res.render("./courses.ejs", { allCourses });
+});
+
+app.get("/courses/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        const allSubjects = await subjects.find({ course: id });
+        res.render("./subjects.ejs", { allSubjects });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred while fetching subjects");
+    }
+});
+
+app.get("/TestApp/:id", async (req, res) => {
+    let { id } = req.params;
+    const allSubjects = await subjects.find({});
+    res.render("./testOrPractice.ejs", { allSubjects , id});
+});
+
+app.get("/subjects/:id", async (req, res) => {
+    try {
+        let { id } = req.params;
+        res.render("./cards.ejs",{id});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred while fetching subjects");
+    }
 });
 
 app.get('/new', (req, res) => {
     res.render('new');
 });
 
-app.get("/practice/allSubjects", async (req, res) => {
-    const allSubjects = await questions.distinct('subject');
-    res.render("./allSubjects.ejs", { allSubjects });
-});
+
 
 app.post("/questions", async (req, res) => {
     // Create a new question document with the data from the request body
