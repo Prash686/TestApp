2ab17e43adca9959ba2794359e0d3871a708e33e
Prash <gsa115376@gmail.com>
2024-09-19 18:49:05 +0530

    Handled all the errors

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const questions = require("./init/question.js");
-const subjects = require("./init/subjects.js");
-const courses = require("./init/course.js");
+const mongoose = require('mongoose');
+const questions = require('./init/question.js');
+const subjects = require('./init/subjects.js');
+const courses = require('./init/course.js');
 const ExpressError = require('./util/ExpressError.js');
-const flash = require("connect-flash");
-const ejs = require("ejs");
-const ejsMate = require("ejs-mate");
-const path = require("path");
+const flash = require('connect-flash');
+const ejsMate = require('ejs-mate');
+const path = require('path');
 
-const Mongo = "mongodb://127.0.0.1:27017/TestApp";
-main().then(() => {
-    console.log("connected");
-}).catch(err => {
-    console.log(err);
-});
-
+// MongoDB connection string from environment variables
+const Mongo = "mongodb+srv://prash:prash%4011@cluster0.p4iok.mongodb.net/myDatabase?retryWrites=true&w=majority";
+console.log(Mongo);
 async function main() {
-    await mongoose.connect(Mongo);
+    try {
+        await mongoose.connect(Mongo);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err);
+    }
 }
 
+main();
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine('ejs', ejsMate);
@@ -28,25 +32,42 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Routes
 app.get("/", async (req, res) => {
     res.render("testapp/home.ejs");
 });
 
 app.get("/practice/:id", async (req, res) => {
     let { id } = req.params;
-    const allquestions = await questions.find({subject: id});
+    const allquestions = await questions.find({ subject: id });
+    
+    if (allquestions.length === 0) {
+        return res.status(404).send("No questions found for this subject");
+    }
+
     res.render("testapp/practice.ejs", { allquestions });
 });
 
 app.get("/test/:id", async (req, res) => {
     let { id } = req.params;
-    const examquestions = await questions.find({subject: id});
-    let max = examquestions.length;
-    const allquestions = [];
-    for (i = 0; i < 20; i++) {
-        let randomInteger = Math.floor(Math.random() * max);
-        allquestions.push(examquestions[randomInteger]);
+    const examquestions = await questions.find({ subject: id });
+    const max = examquestions.length;
+    
+    if (max < 20) {
+        return res.status(400).send("Not enough questions to generate the test.");
     }
+    
+    const allquestions = [];
+    const selectedIndexes = new Set();
+    
+    while (allquestions.length < 20) {
+        let randomInteger = Math.floor(Math.random() * max);
+        if (!selectedIndexes.has(randomInteger)) {
+            allquestions.push(examquestions[randomInteger]);
+            selectedIndexes.add(randomInteger);
+        }
+    }
+    
     res.render("testapp/test.ejs", { allquestions });
 });
 
@@ -94,6 +115,7 @@ app.get('/new', (req, res) => {
 app.post("/questions", async (req, res) => {
     const que = req.body;
     const newQuestion = new questions(que);
+    
     try {
         await newQuestion.save();
         res.send("Form received and question saved!");
@@ -103,15 +125,18 @@ app.post("/questions", async (req, res) => {
     }
 });
 
+// Catch-all route for undefined routes
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 });
 
+// Global error handler
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
     res.status(statusCode).render("testapp/error.ejs", { message });
 });
 
+// Start the server
 app.listen(8080, () => {
-    console.log("success");
+    console.log("Server is running on port 8080");
 });
--- init/index.js
+++ init/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const question = require("./question.js");
+const Questions = require("./question.js");
 
-const MONGO_URL = "mongodb+srv://pravin686:prash%4011@cluster0.yilx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = "mongodb+srv://prash:prash%4011@cluster0.p4iok.mongodb.net/myDatabase?retryWrites=true&w=majority";
 
 main()
   .then(() => {
@@ -17,8 +17,8 @@ async function main() {
 }
 
 const initDB = async () => {
-  await question.deleteMany({});
-  await question.insertMany(initData.data);
+  //await subjects.deleteMany({});
+  await Questions.insertMany(initData.data);
   console.log("data was initialized");
 };
 
